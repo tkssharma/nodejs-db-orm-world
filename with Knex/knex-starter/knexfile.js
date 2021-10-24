79a8d3d8e0dbcb506602b7ff5195619cdc518380
tkssharma <tarun.softengg@gmail.com>
2021-10-24 17:48:09 +0530

    feat: 🎸 moved docker files to outside and added knex app

---
+++ with Knex/knex-starter/knexfile.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    searchPath: ['knex', 'public'],
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+
+  testing: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    searchPath: ['knex', 'public'],
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL,
+    searchPath: ['knex', 'public'],
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+};
